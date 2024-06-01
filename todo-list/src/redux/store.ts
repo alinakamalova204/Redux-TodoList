4f1aeb249d4174fdde010a096ee898e6d5065205
alinakamalova204 <alinakamalova919@gmail.com>
2024-06-01 12:12:03 +0300

    update

--- todo-list/src/redux/store.ts
+++
@@ -1,7 +0,0 @@
-import {createStore} from 'redux';
-import {rootReducer} from './rootReducer';
-
-export const store = createStore(
-    rootReducer,
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-)
